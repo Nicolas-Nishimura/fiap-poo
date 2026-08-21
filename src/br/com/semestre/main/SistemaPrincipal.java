fdbe3d623cbfd71fbbf9762896a555bb70b509f6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-20 21:12:22 -0300

    feat: adiciona projeto atualizado e diagrama de classes do Astah

---
+++ src/br/com/semestre/main/SistemaPrincipal.java
@@ -0,0 +1,27 @@
+package br.com.semestre.main;
+import br.com.semestre.model.Mouse;
+
+public class SistemaPrincipal {
+	public static void main(String[] args) {
+		Mouse meuMouse = new Mouse();
+		meuMouse.cor = "Preto";
+		meuMouse.tamanho = 50.0;
+		meuMouse.marca = "Dell";
+		
+		Mouse mouseCasa = new Mouse();
+		mouseCasa.cor = "Preto";
+		mouseCasa.tamanho = 250.0;
+		mouseCasa.marca = "ATK";
+		
+		System.out.println("Mouse da faculdade: " + meuMouse.tamanho);
+		System.out.println("Mouse de casa: " + mouseCasa.tamanho);
+		
+		System.out.println("\n--- TESTES ---");
+
+		meuMouse.mudarCor("Azul");
+		meuMouse.mudarCor("");    
+		
+		meuMouse.ajustarDpi(800);  
+		meuMouse.ajustarDpi(32000);
+	}
+}
---
+++ src/br/com/semestre/model/Mouse.java
@@ -0,0 +1,28 @@
+package br.com.semestre.model;
+
+public class Mouse {
+	public String cor;
+	public double tamanho; 
+	public String marca;
+	public double dpi = 800.0;  
+	
+	public void mudarCor(String novaCor) {
+		
+		if (novaCor != null && !novaCor.trim().isEmpty()) {
+			this.cor = novaCor;
+			System.out.println("Sucesso: A cor do mouse foi alterada para " + this.cor);
+		} else {
+			System.out.println("Erro: A cor informada é inválida. O estado não foi alterado.");
+		}
+	}
+	
+	public void ajustarDpi(double novoDpi) {
+		
+		if (novoDpi >= 400 && novoDpi <= 16000) {
+			this.dpi = novoDpi;
+			System.out.println("Sucesso: DPI ajustado para " + this.dpi);
+		} else {
+			System.out.println("Erro: DPI " + novoDpi + " é inválido. Escolha um valor entre 400 e 16000.");
+		}
+	}
+}
