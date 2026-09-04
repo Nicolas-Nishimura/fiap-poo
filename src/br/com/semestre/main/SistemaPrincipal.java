71debd5a48635d9dc8e340f4eb3aaabea1edbf4d
Nicolas Sakaue Nishimura <nicolassakaue@gmail.com>
2026-09-04 17:51:42 -0300

    refactor: aplica encapsulamento na classe Passageiro e Objeto Pessoal

--- src/br/com/semestre/main/SistemaPrincipal.java
+++ src/br/com/semestre/main/SistemaPrincipal.java
@@ -1,25 +1,34 @@
 package br.com.semestre.main;
+
 import br.com.semestre.model.Mouse;
+
 public class SistemaPrincipal {
 	public static void main(String[] args) {
+
 		Mouse meuMouse = new Mouse();
-		meuMouse.cor = "Preto";
-		meuMouse.tamanho = 50.0;
-		meuMouse.marca = "Dell";
-		
+		meuMouse.setCor("Preto");
+		meuMouse.setTamanho(50.0);
+		meuMouse.setMarca("Dell");
+
 		Mouse mouseCasa = new Mouse();
-		mouseCasa.cor = "Preto";
-		mouseCasa.tamanho = 250.0;
-		mouseCasa.marca = "ATK";
-		
-		System.out.println("Mouse da faculdade: " + meuMouse.tamanho);
-		System.out.println("Mouse de casa: " + mouseCasa.tamanho);
-		
+		mouseCasa.setCor("Preto");
+		mouseCasa.setTamanho(250.0);
+		mouseCasa.setMarca("ATK");
+
+		System.out.println("Mouse da faculdade: " + meuMouse.getTamanho());
+		System.out.println("Mouse de casa: " + mouseCasa.getTamanho());
+
 		System.out.println("\n--- TESTES ---");
-		meuMouse.mudarCor("Azul");
-		meuMouse.mudarCor("");    
-		
-		meuMouse.ajustarDpi(800);  
-		meuMouse.ajustarDpi(32000);
+		meuMouse.setCor("Azul");
+		meuMouse.setCor("");        
+
+		meuMouse.setDpi(800);
+		meuMouse.setDpi(32000);    
+
+		mouseCasa.setTamanho(-10);  
+
+		System.out.println("\n--- Tentando burlar o sistema ---");
+
+		System.out.println("A proteção funcionou: atributos privados não podem ser acessados diretamente.");
 	}
 }
--- src/br/com/semestre/model/Mouse.java
+++ src/br/com/semestre/model/Mouse.java
@@ -1,12 +1,17 @@
 package br.com.semestre.model;
+
 public class Mouse {
-	public String cor;
-	public double tamanho; 
-	public String marca;
-	public double dpi = 800.0;  
-	
-	public void mudarCor(String novaCor) {
-		
+
+	private String cor;
+	private double tamanho;
+	private String marca;
+	private double dpi = 800.0;
+
+	public String getCor() {
+		return this.cor;
+	}
+
+	public void setCor(String novaCor) {
 		if (novaCor != null && !novaCor.trim().isEmpty()) {
 			this.cor = novaCor;
 			System.out.println("Sucesso: A cor do mouse foi alterada para " + this.cor);
@@ -14,14 +19,45 @@ public class Mouse {
 			System.out.println("Erro: A cor informada é inválida. O estado não foi alterado.");
 		}
 	}
-	
-	public void ajustarDpi(double novoDpi) {
-		
+
+	public double getTamanho() {
+		return this.tamanho;
+	}
+
+	public void setTamanho(double novoTamanho) {
+		if (novoTamanho > 0) {
+			this.tamanho = novoTamanho;
+		} else {
+			System.out.println("Erro: Tamanho inválido. Deve ser maior que 0.");
+		}
+	}
+
+	public String getMarca() {
+		return this.marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getDpi() {
+		return this.dpi;
+	}
+
+	public void setDpi(double novoDpi) {
 		if (novoDpi >= 400 && novoDpi <= 16000) {
 			this.dpi = novoDpi;
 			System.out.println("Sucesso: DPI ajustado para " + this.dpi);
 		} else {
 			System.out.println("Erro: DPI " + novoDpi + " é inválido. Escolha um valor entre 400 e 16000.");
 		}
+	}
+
+	public void mudarCor(String novaCor) {
+		setCor(novaCor);
+	}
+
+	public void ajustarDpi(double novoDpi) {
+		setDpi(novoDpi);
 	}
 }
